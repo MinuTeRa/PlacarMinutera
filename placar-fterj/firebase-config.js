d8e2faa315c40f0d4a22f3635a8286275d8e49ae
MinuTeRa <thiago1.elb@gmail.com>
2026-08-26 09:47:21 -0300

    Update firebase-config.js

--- placar-fterj/firebase-config.js
+++ placar-fterj/firebase-config.js
@@ -1,13 +1,16 @@
-// TROCA SÓ AQUI
-const NOME_BANCO="FTERJ";
-const NOME_CLUBE="FTERJ";
-const NOME_CAMPEONATO="3x7 MinuT&Ra";
-const QTD_LOGOS=2;
-const LOGOS=["logo1.png","logo2.png"];
-let RODAPE_FONTE_PROP=52;
-let RODAPE_FONTE_CONTATO=36;
-const CHAVE_SENHA='CONFIG_SENHA_OPERADOR_'+NOME_BANCO;
-const CHAVE_RODAPE='CONFIG_RODAPE_GLOBAL';
-const CHAVE_FULL='CONFIG_FULL_GLOBAL';
-const CHAVE_TEMPOS='CONFIG_TEMPOS_GLOBAL';
-const firebaseConfig={apiKey:"AIzaSyCTEXeRjxq0_3doBNyWm5AkQ0kmBXmjlSk",authDomain:"placar-minutera.firebaseapp.com",databaseURL:"https://placar-minutera-default-rtdb.firebaseio.com",projectId:"placar-minutera",storageBucket:"placar-minutera.firebasestorage.app",messagingSenderId:"689991648080",appId:"1:689991648080:web:9deeb958183eae23e255"};
+const NOME_BANCO = "FTERJ";
+const NOME_CAMPEONATO = "3x7 MinuT&Ra";
+const NOME_CLUBE = "FTERJ";
+const LOGOS = ["https://i.imgur.com/SEU_LOGO1.png","https://i.imgur.com/SEU_LOGO2.png"];
+const QTD_LOGOS = 2;
+const CHAVE_RODAPE = "PATROCINADORES_RODAPE_" + NOME_BANCO;
+const CHAVE_SENHA_FIREBASE = 'CONFIG_SENHA_OPERADOR_' + NOME_BANCO;
+const firebaseConfig = {
+  apiKey: "AIzaSyCTEXeRjxq0_3doBNyWm5AkQ0kmBXmjlSk",
+  authDomain: "placar-minutera.firebaseapp.com",
+  databaseURL: "https://placar-minutera-default-rtdb.firebaseio.com",
+  projectId: "placar-minutera",
+  storageBucket: "placar-minutera.firebasestorage.app",
+  messagingSenderId: "689991648080",
+  appId: "1:689991648080:web:9deeb958183eae23e255"
+};
